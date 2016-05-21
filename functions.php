5f8054a7a39983e43ea961396747970a60ab7072
Khoi Nguyen <khoinm@joomsolutions.com>
2016-05-21 18:39:05 +0700

    Adding custom theme support feature

--- functions.php
+++ functions.php
@@ -1,16 +1,26 @@
 <?php
-    if ( ! function_exists( 'mytheme_custom_logo' ) ) {
-        function mytheme_custom_logo() {
-            $output = '';
-            $custom_logo_id = get_theme_mod( 'custom_logo' );
-            // Display logo in full size
-            if (function_exists('get_custom_logo'))
-                // You can add class or id to <a href=""> as well.
-                $output = wp_get_attachment_image($custom_logo_id, 'full', false, array('class' => 'navbar-brand'));
-            // Display site title if no logo exists
-            if (empty($output))
-                $output = '<h1><a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h1>';
-            echo $output;
-        }
-    }
+	function mhsplanner_setup() {
+	    add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 75,
+			'flex-height' => true,
+		) );
+		add_theme_support( 'post-formats', array('gallery', 'video') );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
+
+	}
+	add_action( 'after_setup_theme', 'mhsplanner_setup' );
+
+	if ( ! function_exists( 'mytheme_custom_logo' ) ) {
+		function mytheme_custom_logo() {
+		    $output = '';
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+		    if (function_exists('get_custom_logo'))
+		        $output = wp_get_attachment_image($custom_logo_id, 'full', false, array('class' => 'navbar-brand'));
+		    if (empty($output))
+		        $output = '<h1><a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h1>';
+		    echo $output;
+		}
+	}
 ?>
